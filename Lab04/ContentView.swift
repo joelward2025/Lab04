21a6be26e884bd2097f4b68b075fe6e8fa9ea723
joelward2025 <104170342+joelward2025@users.noreply.github.com>
2024-09-13 14:06:50 -0400

    Initial Commit

---
+++ Lab04/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab04
+//
+//  Created by Joel Ward on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab04/Lab04App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab04App.swift
+//  Lab04
+//
+//  Created by Joel Ward on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
