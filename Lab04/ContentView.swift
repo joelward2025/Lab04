1a8e914711f93bcf01fac431015117b63d2b79dd
joelward2025 <104170342+joelward2025@users.noreply.github.com>
2024-09-16 22:27:38 -0400

    init

--- Lab04/ContentView.swift
+++ Lab04/ContentView.swift
@@ -7,15 +7,51 @@
 
 import SwiftUI
 
+var cards = ["A❤️", "2❤️", "3❤️", "4❤️", "5❤️", "6❤️", "7❤️", "8❤️", "9❤️", "10❤️", "J❤️", "Q❤️", "K❤️","A♣️", "2♣️", "3♣️", "4♣️", "5♣️", "6♣️", "7♣️", "8♣️", "9♣️", "10♣️", "J♣️", "Q♣️", "K♣️","A♦️", "2♦️", "3♦️", "4♦️", "5♦️", "6♦️", "7♦️", "8♦️", "9♦️", "10♦️", "J♦️", "Q♦️", "K♦️","A♠️", "2♠️", "3♠️", "4♠️", "5♠️", "6♠️", "7♠️", "8♠️", "9♠️", "10♠️", "J♠️", "Q♠️", "K♠️"]
+
+struct CardView: View {
+    @State private var hovering = false
+    var suitRank: String
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+                .aspectRatio(CGSize(width: 2, height: 3), contentMode: .fill)
+                .foregroundStyle(.white)
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(lineWidth: 3)
+                        .foregroundColor(hovering ? .blue : .black))
+            Text(suitRank)
+                .font(.largeTitle)
+            VStack {
+                HStack {
+                    Text(suitRank)
+                        .padding(5)
+                    Spacer()
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    Text(suitRank)
+                        .padding(5)
+                        .rotationEffect(Angle(degrees: 180))
+                }
+            }
+        }
+        .onTapGesture {
+            hovering = !hovering
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView{
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], content: {
+                ForEach(cards.shuffled(), id: \.self) { item in
+                    CardView(suitRank: item)
+                }
+            }).padding()
         }
-        .padding()
     }
 }
 
